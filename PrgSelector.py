760fad790cb00aa4b1f16dfe056cd388895a60ae
SentientToast1 <dawonder113@gmail.com>
2024-01-21 23:55:58 +0530

    --

--- PrgSelector.py
+++ PrgSelector.py
@@ -7,3 +7,6 @@ match PrgChoice:
     case 2:
         with open("PyScripts\ArrayProbability.py") as prg:
             exec(prg.read())
+    case 3:
+        with open("PyScripts\Gacha.py") as prg:
+            exec(prg.read())    
