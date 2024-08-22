6350efcbfff49535e11db5d46aee60c889fad056
SentientToast1 <dawonder113@gmail.com>
2024-08-22 22:05:05 +0530

    --

--- BinToDec.py
+++ BinToDec.py
@@ -1,27 +1,23 @@
-
 while True:
-    bin = input("Enter binary: ")
-    for x in bin:
-        y = True
-        if x == '0' or x == '1':
-            pass
-        else:
-            y = False
-    if y:
+    binIn = input("Enter binary: ")
+    if all(x in '01' for x in binIn):
         break
     else:
-        print("incorrect")
+        print("Incorrect")
 
 
 
-binList = [int(x) for x in bin]
-compList = []
-for x in range(len(binList)):
-    compList.append(int(pow(2,x)))
-compList.reverse()
-sum = 0
-for x in range(len(binList)):
-    sum += compList[x] * binList[x]
+def BinToDec(bin):
+    binList = [int(x) for x in bin]
+    compList = []
+    for x in range(len(binList)):
+        compList.append(int(pow(2,x)))
+    compList.reverse()
+    sum = 0
+    for x in range(len(binList)):
+        sum += compList[x] * binList[x]
+    return sum
 
 
-print(f"Decimal is: {sum}")
+
+print(f"Decimal is: {BinToDec(binIn)}")
